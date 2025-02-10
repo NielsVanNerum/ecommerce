fe3744e7f04afd042406dddf85c4f0620309d1a1
NielsVanNerum <r0894354@student.thomasmore.be>
2025-02-10 22:46:23 +0100

    fixed nested repos

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup.kumar@luv2code.com", 50000),
+    new SalesPerson("John", "Doe", "john.doe@luv2code.com", 60000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@luv2code.com", 70000),
+    new SalesPerson("Mai", "Truong", "mai.truong@luv2code.com", 80000)
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
